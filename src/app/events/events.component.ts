2f3ef5babad76002b012754faaeb1b84901ef234
bbosak1 <bbosak1@live.maryville.edu>
2020-12-04 21:57:56 -0600

    Add ionic card; image, font updates

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -7,10 +7,15 @@ import { InputDialogServiceProvider } from '../providers/input-dialog-service/in
   templateUrl: 'events.component.html',
   styles: [
     'h1 { font-weight: heavy; }',
-    '.edit-button { background-color: blue; }',
-    'footer { position: fixed; left: 0; bottom: 0; width: 100%; color: white; text-align: center; }', // background-color: #85DCBA;
+    '.edit-button { background-color: #85DCBA }',
+    '.remove-button { background-color: #E27D60 }',
+    'footer { position: fixed; left: 0; bottom: 0; width: 100%; background-color: white; text-align: center; }', // background-color: #85DCBA;
     '.tabs { display: inline-flex !important; }',
     '.tab-icon { padding-left: 8px; padding-right: 8px; }',
+    '.edit-delete-buttons { position: absolute; right: 3px; }',
+    'table, th, td { border: 1px solid black; }',
+    'html, body { font-family: Muli }',
+    'ion-fab-button { --background: #41B3A3; }',
   ]
 })
 export class Events {
